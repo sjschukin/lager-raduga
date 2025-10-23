1dabd4e6999edc3d22ca458c77d41533194f1963
Sergey Shchukin <s.j.schukin@yandex.ru>
2025-10-23 23:15:37 +0300

    feat: add swiper

--- web/themes/custom/cheerful_tan/js/main.js
+++ web/themes/custom/cheerful_tan/js/main.js
@@ -1,4 +1,107 @@
-(function() {
+(function () {
   "use strict";
+
+  //>> Team Slider Start <<//
+  const teamSlider = new Swiper(".team-slider", {
+    spaceBetween: 30,
+    speed: 1300,
+    loop: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".array-prev",
+      prevEl: ".array-next",
+    },
+    pagination: {
+      el: ".dot",
+      clickable: true,
+    },
+    breakpoints: {
+      1299: {
+        slidesPerView: 4,
+      },
+      1199: {
+        slidesPerView: 3,
+      },
+      767: {
+        slidesPerView: 2,
+      },
+      575: {
+        slidesPerView: 2,
+      },
+      0: {
+        slidesPerView: 1,
+      },
+    },
+  });
+
+  //>> Testimonial Slider Start <<//
+  const testimonialSlider = new Swiper(".testimonial-slider", {
+    spaceBetween: 30,
+    speed: 1500,
+    loop: true,
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".dot",
+      clickable: true,
+    },
+    breakpoints: {
+      1199: {
+        slidesPerView: 3,
+      },
+      767: {
+        slidesPerView: 2,
+      },
+      575: {
+        slidesPerView: 1,
+      },
+      0: {
+        slidesPerView: 1,
+      },
+    },
+  });
+
+  //>> Last Photos Slider Start <<//
+  const instagramBannerSlider = new Swiper(".last-photos-banner-slider", {
+    spaceBetween: 30,
+    speed: 1500,
+    loop: true,
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".array-prev",
+      prevEl: ".array-next",
+    },
+    breakpoints: {
+      1399: {
+        slidesPerView: 6,
+      },
+      1199: {
+        slidesPerView: 5,
+      },
+      991: {
+        slidesPerView: 4,
+      },
+      767: {
+        slidesPerView: 3,
+      },
+      650: {
+        slidesPerView: 2,
+      },
+      575: {
+        slidesPerView: 1,
+      },
+      0: {
+        slidesPerView: 1,
+      },
+    },
+  });
 
 })();
